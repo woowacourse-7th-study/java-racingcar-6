17dca371765f82e97d92b55bdf66aaa553c48ed9
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-09-04 13:39:52 +0900

    feat(OutputView): OutputView 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,13 +2,15 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.service.UserInputValidator;
+import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        OutputView.inputCarNames();
         String carNames = Console.readLine();
         UserInputValidator.validateCarNames(carNames);
-        System.out.println("시도할 회수는 몇회인가요?");
+
+        OutputView.inputRounds();
         String rounds = Console.readLine();
         UserInputValidator.validateRounds(rounds);
     }
