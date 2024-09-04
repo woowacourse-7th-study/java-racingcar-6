05d27cedda020b8dac33b094ebdc4f7ce734a058
JHZLO <010703kjh@gamil.com>
2024-09-04 16:15:01 +0900

    feat(Car): Car 클래스 생성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,26 @@
 package racingcar.model;
 
 public class Car {
+    private static final int MOVE_FORWARD_LOWER_BOUND = 4; // 전진 조건
+    private final String name;
+    private int distance = 0;
+
+    public Car(final String name) {
+        this.name = name;
+    }
+
+    public void moveForward(final int condition) { // 전진 조건
+        if (condition >= MOVE_FORWARD_LOWER_BOUND) {
+            distance++;
+        }
+    }
+
+    public String getCarName() { // carName 반환
+        return this.name;
+    }
+
+    public Integer getDistance() { // 현재 거리 반환
+        return this.distance;
+    }
+
 }
