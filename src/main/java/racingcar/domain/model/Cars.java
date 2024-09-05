38a22563d5bdc145e29e1da588258f424ef8e97f
JHZLO <010703kjh@gamil.com>
2024-09-06 02:58:51 +0900

    refactor(Cars): 변수명 수정

--- src/main/java/racingcar/domain/model/Cars.java
+++ src/main/java/racingcar/domain/model/Cars.java
@@ -1,34 +1,33 @@
 package racingcar.domain.model;
 
 import racingcar.domain.Movable;
-import racingcar.domain.model.Car;
 
 import java.util.List;
 
 public class Cars {
-    private final List<Car> racingCars;
+    private final List<Car> carList;
 
-    public Cars(final List<Car> racingCars) {
-        this.racingCars = racingCars;
+    public Cars(final List<Car> carList) {
+        this.carList = carList;
     }
 
     public void updateRaceRound(Movable movable) {
-        racingCars.forEach(car -> car.moveForward(movable));
+        carList.forEach(car -> car.moveForward(movable));
     }
 
     public List<Car> getCars() {
-        return racingCars;
+        return carList;
     }
 
     public int getMaxDistance() {
-        return racingCars.stream()
+        return carList.stream()
                 .mapToInt(Car::getDistance)
                 .max()
                 .orElse(0);
     }
 
     public List<Car> getWinners(final int maxDistance) {
-        return racingCars.stream()
+        return carList.stream()
                 .filter(car -> car.getDistance() == maxDistance)
                 .toList();
     }
